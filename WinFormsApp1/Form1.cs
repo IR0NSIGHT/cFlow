4466ff8837f656ac8420d7575e1f15b0fe113a6f
IR0NSIGHT <irnsght@googlemail.com>
2024-02-01 14:35:08 +0100

    zoomable picutre of flowmap

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -6,7 +6,7 @@ namespace WinFormsApp1
     public partial class MainWindow : Form
     {
         private (int x, int y) currentCenter;
-        private float scale;
+        private float scale = 1;
         private bool isDragging;
         System.Drawing.Point originalMousePos;
 
@@ -21,9 +21,22 @@ namespace WinFormsApp1
             var path = "C:\\Users\\Max1M\\OneDrive\\Bilder\\cFlow\\";
             var file = "medium_flats.png";
             var gen = new cFlowApi.CFlowGenerator(path + file);
+            //    pictureBox1.Image = SkiaSharp.Views.Desktop.Extensions.ToBitmap(gen.HeightmapImg);
             ShowMyImage(pictureBox1, SkiaSharp.Views.Desktop.Extensions.ToBitmap(gen.HeightmapImg));
             ShowMyImage(pictureBox2, SkiaSharp.Views.Desktop.Extensions.ToBitmap(gen.FlowmapImgColored));
             ShowMyImage(pictureBox3, SkiaSharp.Views.Desktop.Extensions.ToBitmap(gen.RivermapImg));
+
+            pictureBox2.Paint += PictureBox1_Paint;
+            this.MouseWheel += MainForm_MouseWheel;
+        }
+
+        private void MainForm_MouseWheel(object? sender, MouseEventArgs e)
+        {
+            // Handle the mouse wheel event here
+            int delta = e.Delta; // Delta represents the amount the wheel was rotated
+                                 // Perform actions based on the delta value, for example, zooming or scrolling
+            scale += (delta*1f/1000);
+            pictureBox2.Invalidate(); // Force PictureBox to repaint
         }
 
         private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
@@ -56,11 +69,6 @@ namespace WinFormsApp1
             }
         }
 
-        private void PictureBox1_Paint(object sender, PaintEventArgs e)
-        {
-            // You can leave this event empty or handle additional painting logic if needed
-        }
-
         public static void ShowMyImage(PictureBox box, Bitmap skBitmap)
         {
             var (xSize, ySize) = (box.Width, box.Height);
@@ -89,12 +97,39 @@ namespace WinFormsApp1
 
             // Stretches the image to fit the pictureBox.
             box.SizeMode = PictureBoxSizeMode.StretchImage;
-           
+
             box.ClientSize = new Size(newWidth, newHeight);
 
             box.Image = (Image)skBitmap;
 
         }
+
+        private void PictureBox1_Paint(object? sender, PaintEventArgs e)
+        {
+            if (pictureBox2.Image != null)
+            {
+                // Calculate the portion of the image to display
+                Rectangle displayRect = new Rectangle(
+                   0, 0,
+                    pictureBox2.Width,
+                    pictureBox2.Height);
+
+                using (Brush orangeBrush = new SolidBrush(Color.Orange))
+                {
+                    e.Graphics.FillRectangle(orangeBrush, 0, 0, pictureBox2.Width, pictureBox2.Height);
+                }
+
+                // Draw the portion of the image within the calculated rectangle
+                e.Graphics.DrawImage(
+                    pictureBox2.Image,
+                    displayRect,
+                    new Rectangle(0, 0, (int)(pictureBox2.Image.Width / scale), (int)(pictureBox2.Image.Height / scale)),
+                    GraphicsUnit.Pixel);
+
+
+            }
+        }
+
 
         /// <summary>
         /// image has oldPos with oldSize. redraw image to another size, have to recalc position so the new image is centered to the old images center
