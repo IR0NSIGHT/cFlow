7d71830e126c877d36d6e59ea754871fcc511cd4
IR0NSIGHT <irnsght@googlemail.com>
2024-02-04 12:20:19 +0100

    move Map2D interface to own file

--- cFlowAPI/Maps/heightMap/IHeightMap.cs
+++ cFlowAPI/Maps/heightMap/IHeightMap.cs
@@ -3,11 +3,3 @@
     short GetHeight(int x, int y);
     void SetHeight((int x, int y) pos, short z);
 }
-
-public interface Map2d
-{
-    (int x, int y) Bounds();
-    bool inBounds(int x, int y);
-
-    IMapIterator<(int x, int y)> iterator();
-}
---
+++ cFlowAPI/Maps/heightMap/Map2d.cs
@@ -0,0 +1,7 @@
+﻿public interface Map2d
+{
+    (int x, int y) Bounds();
+    bool inBounds(int x, int y);
+
+    IMapIterator<(int x, int y)> iterator();
+}
