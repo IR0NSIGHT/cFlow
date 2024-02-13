e448211209daa88c2c0728767a1ff96f0ffa68a5
IR0NSIGHT <irnsght@googlemail.com>
2024-02-13 20:32:42 +0100

    super basic baby steps, its breaking me

---
+++ WpfApp1/MapViewControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MapViewControl.xaml
+    /// </summary>
+    public partial class MapViewControl : UserControl
+    {
+        public MapViewControl()
+        {
+            InitializeComponent();
+
+
+        }
+
+    
+    }
+
+}
---
+++ WpfApp1/components/TierCard.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.components
+{
+    /// <summary>
+    /// Interaction logic for TierCard.xaml
+    /// </summary>
+    public partial class TierCard : UserControl
+    {
+        public TierCard()
+        {
+            InitializeComponent();
+
+            originalImage = new BitmapImage(new Uri("C:\\Users\\Max1M\\OneDrive\\Bilder\\cFlow\\textureGrid_1k.jpg", UriKind.Absolute));
+            _currentMapSection = new MapSection(0, 0, originalImage.PixelWidth);
+            this.Loaded += RedrawMap;
+        }
+
+        private BitmapImage originalImage;
+
+        readonly struct MapSection
+        {
+            public readonly int PosX;
+            public readonly int PosY;
+            public readonly int DisplayWidth;
+
+            public MapSection(int posX, int posY, int displayWidth) =>
+                (PosX, PosY, DisplayWidth) = (posX, posY, displayWidth);
+        }
+
+        private MapSection _currentMapSection = new();
+        private Int32Rect getDisplayMapCrop(double guiWidth, double guiHeight) => new Int32Rect(
+            this._currentMapSection.PosX,
+            this._currentMapSection.PosY,
+            this._currentMapSection.DisplayWidth,
+            (int)(_currentMapSection.DisplayWidth * (guiHeight / (guiWidth)))
+        );
+
+        private void RedrawMap(object sender, RoutedEventArgs e)
+        {
+            // Load the original image
+
+            var section = getDisplayMapCrop(this.ActualWidth, this.ActualHeight);
+            CroppedBitmap cropped_bitmap =
+                new CroppedBitmap(originalImage, section );
+
+            MapImage.Source = cropped_bitmap;
+        }
+    }
+}
