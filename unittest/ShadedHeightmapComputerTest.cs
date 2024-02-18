533dfa6ad093a06f6ab260a2bdd866c3f706f855
IR0NSIGHT <irnsght@googlemail.com>
2024-02-18 13:07:40 +0100

    improved sunlight shader

--- unittest/ShadedHeightmapComputerTest.cs
+++ unittest/ShadedHeightmapComputerTest.cs
@@ -9,7 +9,7 @@ public class ShadedHeightmapComputerTest
     [Test]
     public void owo()
     {
-        Bitmap bitmap = new Bitmap("C:\\Users\\Max1M\\OneDrive\\Bilder\\cFlow\\medium_flats_gray8bpp.png");
+        Bitmap bitmap = new Bitmap("C:\\Users\\Max1M\\OneDrive\\Bilder\\cFlow\\large_circle.png");
 
         Bitmap shaded = ShadedHeightmapComputer.RunShader(bitmap);
         shaded.Save("C:\\Users\\Max1M\\OneDrive\\Bilder\\cFlow\\shaded.png", ImageFormat.Png);
