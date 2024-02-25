f9cfb7b95bdeb43b916d860823cde02e90fe5ff1
IR0NSIGHT <irnsght@googlemail.com>
2024-02-25 12:41:40 +0100

    floodshader aborts when it finds the closest escape points

--- cFlowAPI/Maps/Shader/FloodPlaneShader.cs
+++ cFlowAPI/Maps/Shader/FloodPlaneShader.cs
@@ -7,6 +7,7 @@ using application.Maps;
 using application.Maps.flowMap;
 using cFlowApi.Heightmap;
 using ComputeSharp;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace cFlowAPI.Maps.Shader
 {
@@ -22,53 +23,73 @@ namespace cFlowAPI.Maps.Shader
 
         public static Shader FromMaps(DummyDimension heightMap, BooleanMap floodMap, int height)
         {
-            var heightData = GraphicsDevice.GetDefault().AllocateReadOnlyTexture2D<uint>(heightMap.ToGPUdata());
-            var boolDataArray = floodMap.ToGpuData();
-            var boolData = GraphicsDevice.GetDefault().AllocateReadWriteTexture2D<int>(boolDataArray);
             var changedBuffer = GraphicsDevice.GetDefault().AllocateReadWriteBuffer<int>(new int[1]);
 
-            var shader = new FloodPlaneComputer.Shader(heightData, boolData, changedBuffer, (uint) height);
+            var escapePointIdxBuffer = GraphicsDevice.GetDefault().AllocateReadWriteBuffer(new int[1]);
+            var escapePointBuffer = GraphicsDevice.GetDefault().AllocateReadWriteBuffer(new int2[100]);
+            var shader = new FloodPlaneComputer.Shader(
+                GraphicsDevice.GetDefault().AllocateReadOnlyTexture2D<uint>(heightMap.ToGPUdata()),
+                GraphicsDevice.GetDefault().AllocateReadWriteTexture2D<int>(floodMap.ToGpuData()),
+                GraphicsDevice.GetDefault().AllocateReadWriteTexture2D<int>(floodMap.Bounds().x, floodMap.Bounds().y),
+                changedBuffer, (uint)height, escapePointIdxBuffer, escapePointBuffer);
             return shader;
-        }
-
-        public static Shader WithHeightAndUnchanged(int height, Shader shader)
-        {
-            shader.changed.CopyFrom(new int[1]);
-            return new FloodPlaneComputer.Shader(shader.heightTexture, shader.MarkedTexture, shader.changed, 0);
         }
 
         public static BooleanMap MarkedMapFromShader(Shader shader)
         {
-            var booleanMap = new BooleanMap((shader.MarkedTexture.Width, shader.MarkedTexture.Height));
-            booleanMap.FromGpuData(shader.MarkedTexture.ToArray());
+            var booleanMap = new BooleanMap((shader.AfterMarkedTexture.Width, shader.AfterMarkedTexture.Height));
+            booleanMap.FromGpuData(shader.AfterMarkedTexture.ToArray());
             return booleanMap;
         }
 
         public static BooleanMap RunUntilEscapeFoundOrPlaneDone(Shader shader, int maxIterations = 10000)
         {
+            var currentShader = shader;
+
             for (int i = 0; i < maxIterations; i++)
             {
-                GraphicsDevice.GetDefault().For(shader.heightTexture.Width, shader.heightTexture.Height, shader);
-                var didChange = didChangeAndReset(shader);
+                GraphicsDevice.GetDefault().For(shader.heightTexture.Width, shader.heightTexture.Height, currentShader);
+                var didChange = didChangeAndReset(currentShader);
                 if (!didChange)
                     break;
+                if (shader.escapeIdx.ToArray()[0] != 0)
+                    break;
+
+                //apply output floodmap to new input
+                currentShader = new Shader(
+                    currentShader.heightTexture,
+                    currentShader.AfterMarkedTexture,
+                    currentShader.BeforeMarkedTexture,
+                    currentShader.changed,
+                   currentShader.currentHeight,
+                   currentShader.escapeIdx,
+                   currentShader.escapePoints
+                    );
             }
-            var floodMap = new BooleanMap((shader.heightTexture.Width, shader.heightTexture.Height));
-            floodMap.FromGpuData(shader.MarkedTexture.ToArray());
-            return floodMap;
+
+            return MarkedMapFromShader(currentShader);
         }
 
-        public static (int x, int y)[] GetFoundEscapePoints(Shader shader) {
-            return [];
+        public static (int x, int y)[] GetFoundEscapePoints(Shader shader)
+        {
+            var rawPoints = shader.escapePoints.ToArray();
+            Array.Resize(ref rawPoints, shader.escapeIdx.ToArray()[0]);
+            var escapePoints = Array.ConvertAll(rawPoints, new Converter<int2, (int x, int y)>(i2 => (i2.X, i2.Y)));
+            return escapePoints;
         }
 
         [AutoConstructor]
         public partial struct Shader : IComputeShader
         {
             public readonly ReadOnlyTexture2D<uint> heightTexture;
-            public readonly ReadWriteTexture2D<int> MarkedTexture;
+            public readonly ReadWriteTexture2D<int> BeforeMarkedTexture;
+            public readonly ReadWriteTexture2D<int> AfterMarkedTexture;
+
+
             public readonly ReadWriteBuffer<int> changed;
             public readonly uint currentHeight;
+            public readonly ReadWriteBuffer<int> escapeIdx;
+            public readonly ReadWriteBuffer<int2> escapePoints;
 
             public uint getHeight(int2 xy)
             {
@@ -77,14 +98,14 @@ namespace cFlowAPI.Maps.Shader
 
             public bool isMarked(int2 xy)
             {
-                return MarkedTexture[xy] == 1;
+                return BeforeMarkedTexture[xy] == 1;
             }
 
             public int2 neighbour(int x, int y, int2 self)
             {
                 return self + new int2(x, y);
             }
-            public bool FloodedEqualNeighbour(int2 xy)
+            public bool FloodedNeighbourAtCurrentHeight(int2 xy)
             {
                 return xy.X >= 0 &&
                        xy.Y >= 0 &&
@@ -95,20 +116,42 @@ namespace cFlowAPI.Maps.Shader
             }
             public void Execute()
             {
+                if (ThreadIds.X == 0 && ThreadIds.Y == 0)   //reset on each run
+                {
+                    escapeIdx[0] = 0;
+                    changed[0] = 0;
+                }
+
+
                 int2 XY = ThreadIds.XY;
                 uint ownHeight = heightTexture[XY];
+
+                //copy over existing values
+                AfterMarkedTexture[XY] = BeforeMarkedTexture[XY];
+
                 if (
-                    MarkedTexture[XY] == 0 && 
-                    ownHeight == currentHeight &&
+                    BeforeMarkedTexture[XY] == 0 &&
                     (
-                    FloodedEqualNeighbour(neighbour(-1, 0, XY)) ||
-                    FloodedEqualNeighbour(neighbour(1, 0, XY)) ||
-                    FloodedEqualNeighbour(neighbour(0, 1, XY)) ||
-                    FloodedEqualNeighbour(neighbour(0, -1, XY))))
+                    FloodedNeighbourAtCurrentHeight(neighbour(-1, 0, XY)) ||
+                    FloodedNeighbourAtCurrentHeight(neighbour(1, 0, XY)) ||
+                    FloodedNeighbourAtCurrentHeight(neighbour(0, 1, XY)) ||
+                    FloodedNeighbourAtCurrentHeight(neighbour(0, -1, XY))))
                 {
-                    MarkedTexture[XY] = 1;
-                    changed[0] = 1;
+                    if (ownHeight == currentHeight)
+                    {
+                        AfterMarkedTexture[XY] = 1;
+                        changed[0] = 1;
+                    }
+                    else if (ownHeight < currentHeight)
+                    {
+                        //TBD
+                        int idx = 0;
+                        Hlsl.InterlockedAdd(ref escapeIdx[0], 1, out idx);
+                        escapePoints[idx] = ThreadIds.XY;
+                    }
+
                 }
+
             }
         }
     }
--- unittest/Shader/FloodPlaneShaderTest.cs
+++ unittest/Shader/FloodPlaneShaderTest.cs
@@ -9,79 +9,6 @@ namespace unittest.Shader;
 public class FloodPlaneShaderTest
 {
     [Test]
-    public void FloodOneLevelSimpleStepByStep()
-    {
-        DummyDimension heightMap = new DummyDimension((5, 4), 7);
-        heightMap.FromGPUdata(new int[,] {
-                { 7, 7, 7, 7, 7},
-                { 7, 3, 3, 7, 3},
-                { 4, 3, 3, 4, 7},
-                { 5, 3, 3, 6, 6}
-            });
-        BooleanMap booleanMap = new BooleanMap(heightMap.Bounds());
-        booleanMap.setMarked(1, 2);
-
-        //check before running
-        var shouldBeFlooded = new int[,]
-        {
-                { 0, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0 },
-                { 0, 1, 0, 0, 0 },
-                { 0, 0, 0, 0, 0 }
-        };
-        var flooded = booleanMap.ToGpuData();
-        Assert.That(flooded, Is.EqualTo(shouldBeFlooded));
-
-
-        //run flood shader
-        var shader = FloodPlaneComputer.FromMaps(heightMap, booleanMap, 3);
-        GraphicsDevice.GetDefault().For(heightMap.Bounds().x, heightMap.Bounds().y, shader);
-
-        //verify first iteration
-        flooded = FloodPlaneComputer.MarkedMapFromShader(shader).ToGpuData();
-        shouldBeFlooded = new int[,]
-        {
-                { 0, 0, 0, 0, 0 },
-                { 0, 1, 0, 0, 0 },
-                { 0, 1, 1, 0, 0 },
-                { 0, 1, 0, 0, 0 }
-        };
-        Assert.That(flooded, Is.EqualTo(shouldBeFlooded));
-        Assert.That(FloodPlaneComputer.didChangeAndReset(shader));
-
-
-        //run second time
-        GraphicsDevice.GetDefault().For(heightMap.Bounds().x, heightMap.Bounds().y, shader);
-
-        //verify first iteration
-        flooded = FloodPlaneComputer.MarkedMapFromShader(shader).ToGpuData();
-        shouldBeFlooded = new int[,]
-        {
-                { 0, 0, 0, 0, 0 },
-                { 0, 1, 1, 0, 0 },
-                { 0, 1, 1, 0, 0 },
-                { 0, 1, 1, 0, 0 }
-        };
-        Assert.That(flooded, Is.EqualTo(shouldBeFlooded));
-        Assert.That(FloodPlaneComputer.didChangeAndReset(shader));
-
-        //run third time => nothing changed
-        GraphicsDevice.GetDefault().For(heightMap.Bounds().x, heightMap.Bounds().y, shader);
-
-        //verify first iteration
-        flooded = FloodPlaneComputer.MarkedMapFromShader(shader).ToGpuData();
-        shouldBeFlooded = new int[,]
-        {
-                { 0, 0, 0, 0, 0 },
-                { 0, 1, 1, 0, 0 },
-                { 0, 1, 1, 0, 0 },
-                { 0, 1, 1, 0, 0 }
-        };
-        Assert.That(flooded, Is.EqualTo(shouldBeFlooded));
-        Assert.That(FloodPlaneComputer.didChangeAndReset(shader), Is.False);
-    }
-
-    [Test]
     public void FloodOneLevelAuto()
     {
         DummyDimension heightMap = new DummyDimension((5, 4), 7);
@@ -119,17 +46,17 @@ public class FloodPlaneShaderTest
                 { 0, 1, 1, 0, 0 }
         };
         Assert.That(flooded, Is.EqualTo(shouldBeFlooded));
-        
+
     }
     [Test]
-    public void CanFindEscapePoint()
+    public void FindsClosestEscapePoints()
     {
         DummyDimension heightMap = new DummyDimension((5, 4), 7);
         heightMap.FromGPUdata(new int[,] {
                 { 1, 7, 7, 1, 7},
-                { 7, 3, 3, 7, 3},
-                { 7, 3, 3, 7, 7},
-                { 7, 3, 3, 1, 7} /*<= 1 is escape point */
+                { 7, 3, 3, 1, 3},/*B*/
+         /*A*/  { 1, 3, 3, 7, 7},
+                { 7, 3, 3, 1, 7} /*C*/
             });
         BooleanMap booleanMap = new BooleanMap(heightMap.Bounds());
         booleanMap.setMarked(1, 1);
@@ -150,7 +77,14 @@ public class FloodPlaneShaderTest
         var shader = FloodPlaneComputer.FromMaps(heightMap, booleanMap, 3);
         FloodPlaneComputer.RunUntilEscapeFoundOrPlaneDone(shader);
 
-        //TODO 
+        var idx = shader.escapeIdx.ToArray();
+        var escapePoints = Array.ConvertAll(shader.escapePoints.ToArray(), new Converter<int2, (int x, int y)>(i2 => (i2.X, i2.Y)));
+
+        //A and B are found, C is not reached because the shader aborts earlier.
+        CollectionAssert.AreEquivalent(
+            new (int x, int y)[] { (0, 2), (3, 1) },
+            FloodPlaneComputer.GetFoundEscapePoints(shader)
+            );
 
     }
 }
